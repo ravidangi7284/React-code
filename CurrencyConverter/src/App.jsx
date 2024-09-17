bbf90cc42c81271d607973bf73acf91f829c72ed
ravidangi7284 <93817949+ravidangi7284@users.noreply.github.com>
2024-09-17 16:33:25 +0530

    bbb

--- CurrencyConverter/src/App.jsx
+++ CurrencyConverter/src/App.jsx
@@ -29,7 +29,7 @@ function App() {
     <div
         className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
         style={{
-            backgroundImage: `url('https://images.pexels.com/photos/3532540/pexels-photo-3532540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`,
+            backgroundImage: `https://images.pexels.com/photos/3532551/pexels-photo-3532551.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1`,
         }}
     >
         <div className="w-full">
